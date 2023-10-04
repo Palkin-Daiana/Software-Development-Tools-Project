350a90e54dfe4bd6b8484eef1b30c07fea0b9cf3
Palkin-Daiana <soodaiana@gmail.com>
2023-10-04 18:58:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ fig2.for_each_trace(lambda t: t.update(hovertemplate=t.hovertemplate.replace("co
 #fig3_values = filtered_data['fuel'].value_counts().values
 #fig3 = px.pie(data_frame=filtered_data, names=fig3_labels, values=fig3_values, title='A lot of GAZ fueled cars! We don\'t forget the DIESEL lovers', color_discrete_sequence=px.colors.sequential.Rainbow)
 
-count_fuel = df['fuel'].value_counts()
+count_fuel = filtered_data['fuel'].value_counts()
 cols = st.columns(len(count_fuel))
 counter = 0
 for i, v in count_fuel.items():
